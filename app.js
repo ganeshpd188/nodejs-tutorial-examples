aa788080141d4333a0e2c9301fd61eede6430f1c
ganeshpd188 <37778502+ganeshpd188@users.noreply.github.com>
2018-10-24 00:33:19 +0530

    Read and Write Files using fs in noejs
    Synchronous mode

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-var stuff = require('./count');
+var fs = require('fs');
+//read the file
+var readMe = fs.readFileSync('./read.txt', 'utf8');
 
-console.log(stuff.counter(['1','2','3']));
-console.log(stuff.adder(3,4));
-console.log(stuff.adder(stuff.pi,stuff.pi));
+console.log(readMe);
+
+// write to the read
+//creates a new file writeMe.txt and write the contents of readMe into it
+fs.writeFileSync('writeMe.txt',readMe);
+//or
+//fs.writeFileSync('writeMe.txt',' Something else is also written');
